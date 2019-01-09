052cdc79a7a7b069d02a983c9a24963ad6c5344d
Merkle Groot <40680812+merklegroot@users.noreply.github.com>
2019-01-08 23:16:38 -0500

    Add files via upload

---
+++ BlockTradeOrderBook.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace BlocktradeExchangeLib.Models
+{
+    public class BlockTradeOrderBook
+    {
+        [JsonProperty("bids")]
+        public List<BlockTradeOrder> Bids { get; set; }
+
+        [JsonProperty("asks")]
+        public List<BlockTradeOrder> Asks { get; set; }        
+    }
+}
